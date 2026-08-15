f9334f6beeb8c99583e0a1abf315a2d273370217
glacigaymeow <gemcutie25@gmail.com>
2026-08-15 22:54:14 +1200

    Initial Commit

---
+++ src/main/java/net/gaymeow/amends/AmendsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gaymeow.amends;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AmendsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
